d744324133db5b840403c0e8d41858d65e77a35b
Mikail Sheikh <mikail@cogitolearning.co.uk>
2013-11-27 13:53:23 +0000

    Added Property Animations Example

---
+++ PropertyAnimations/src/uk/co/cogitolearning/propertyanimations_example/MainActivity.java
@@ -0,0 +1,33 @@
+package uk.co.cogitolearning.propertyanimations_example;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu)
+  {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+  
+  public void goExample1(View view)
+  {
+    Intent intent = new Intent(this, PropertyAnimation01.class);
+    startActivity(intent);
+  }
+
+}
---
+++ PropertyAnimations/src/uk/co/cogitolearning/propertyanimations_example/PropertyAnimation01.java
@@ -0,0 +1,38 @@
+package uk.co.cogitolearning.propertyanimations_example;
+
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+public class PropertyAnimation01 extends Activity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.property_animations01);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu)
+  {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  public void flipOnVertical(View view)
+  {
+    Log.d("propertyanimations", "flipOnVertical");
+    View image = findViewById(R.id.some_image);
+    Animator anim = AnimatorInflater.loadAnimator(this, R.animator.flip_on_vertical);
+    anim.setTarget(image);
+    anim.start();
+  }
+
+}
